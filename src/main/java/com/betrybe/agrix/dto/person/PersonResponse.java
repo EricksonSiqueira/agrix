64d91bd962e4fc259936150183fe9a193b82393f
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-12-06 17:39:48 -0300

    fix: style check list

--- src/main/java/com/betrybe/agrix/dto/person/PersonResponse.java
+++ src/main/java/com/betrybe/agrix/dto/person/PersonResponse.java
@@ -1,6 +1,8 @@
 package com.betrybe.agrix.dto.person;
 
-import com.betrybe.agrix.models.entities.Person;
 import com.betrybe.agrix.security.Role;
 
+/**
+ * The type Person response.
+ */
 public record PersonResponse(Long id, String username, Role role) { }
